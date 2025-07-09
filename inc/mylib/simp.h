ef79f0a94e3d6ec865dfc18d1bf738bbef2a142c
vijaylaxmiIttannavar <vijaylaxmiittannavar@gmail.com>
2025-07-09 14:08:37 +0530

    update

---
+++ inc/mylib/simp.h
@@ -0,0 +1,7 @@
+//#ifndef SIMP_H
+//#define SIMP_H
+#pragma once
+int add(int, int);
+int sub(int, int);
+
+//#endif
---
+++ out/build/x64-Debug/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: C:/Users/Administrator/Desktop/proj_9date
+# Build directory: C:/Users/Administrator/Desktop/proj_9date/out/build/x64-Debug
+# 
+# This file includes the relevant testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+add_test(test_add01 "C:/Users/Administrator/Desktop/proj_9date/out/build/x64-Debug/test_add01.exe")
+set_tests_properties(test_add01 PROPERTIES  _BACKTRACE_TRIPLES "C:/Users/Administrator/Desktop/proj_9date/CMakeLists.txt;29;add_test;C:/Users/Administrator/Desktop/proj_9date/CMakeLists.txt;33;add_calc_test;C:/Users/Administrator/Desktop/proj_9date/CMakeLists.txt;0;")
